38b36b480359b6dceed61a7e1b359c036e554c21
Omar <omar.garcia@morsa.com.mx>
2025-12-04 19:29:48 -0600

    CReando la ACCION para REINICIAR la app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useEffect } from 'react';
+import { useReducer, useEffect, useMemo } from 'react';
 import Form from './components/Form';
 import { activityReducer, initialState } from './reducers/activity-reducer';
 import ActivityList from './components/ActivityList';
@@ -11,33 +11,37 @@ function App() {
     localStorage.setItem('activities', JSON.stringify(state.activities));
   }, [state.activities]);
 
+  const canRestartApp = () => useMemo( () => state.activities.length, [state.activities]);
+
   return (
     <>
       <header className="bg-lime-600 py-3">
         <div className="max-w-4xl mx-auto flex justify-between">
-          <h1 className="text-center text-lg font-bold text-white uppercase">Contador de Calorías</h1>
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Contador de Calorías
+          </h1>
 
-
+          <button
+            className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10 disabled:cursor-default"
+            disabled={!canRestartApp()}
+            onClick={() => dispatch({type: 'restart-app'})}
+          >
+            Reiniciar App
+          </button>
         </div>
       </header>
 
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form
-            dispatch={dispatch}
-            state={state}
-          />
+          <Form dispatch={dispatch} state={state} />
         </div>
       </section>
 
-      <section className='p-10 mx-auto max-w-4xl'>
-        <ActivityList
-          activities={state.activities}
-          dispatch={dispatch}
-        />
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList activities={state.activities} dispatch={dispatch} />
       </section>
     </>
-  )
+  );
 }
 
 export default App;
